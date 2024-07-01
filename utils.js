b87fa9043bb31852ee50825d84c4092f04950d60
MarkNoppler <Jacobfairhurst1@gmail.com>
2024-07-01 13:53:21 +0100

    Add files via upload
    Initialized Goblin Coffee website by uploading HTML, CSS, JS  and Img files

---
+++ utils.js
@@ -0,0 +1,34 @@
+const btn = document.getElementById("submit-btn");
+let currentSize = 1;
+
+btn.addEventListener("click", () => {
+    alert("The goblins know who you are!");
+});
+
+btn.addEventListener("click", function onclick() {
+    btn.style.backgroundColor = "red";
+    btn.style.color = "#4d693a";
+});
+
+btn.addEventListener("mouseover", () => {
+    currentSize += 1;
+    btn.style.transform = "scale(3)";
+});
+
+btn.addEventListener("mouseout", () => {
+    currentSize = 1;
+    btn.style.transform = "scale(1)";
+});
+
+
+
+document.getElementById("fname").style.height="50px";
+document.getElementById("fname").style.fontSize="22pt";
+document.getElementById("fname").style.padding="5px";
+document.getElementById("fname").style.margin="20px";
+
+document.getElementById("lname").style.height="50px";
+document.getElementById("lname").style.fontSize="22pt";
+document.getElementById("lname").style.padding="5px";
+document.getElementById("lname").style.margin="20px"
+
